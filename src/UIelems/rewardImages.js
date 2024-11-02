dc7b6009f2d06b5d86ff1f81224ed3532654cf63
Óscar Melquiades Durán Narganes <osduran@ucm.es>
2024-11-02 16:47:18 +0100

    Update rewardImages.js

--- src/UIelems/rewardImages.js
+++ src/UIelems/rewardImages.js
@@ -6,13 +6,13 @@ export default class RewardImages extends DescriptionImages{
         this.index = index;
         switch (rclass){
             case RewardClass.instrument:
-                super(scene,x,y,"../../assets/img/i_"+index+".png",InstrumentDataBase[index].nombre, InstrumentDataBase[index].description);
+                super(scene,x,y,"../../assets/img/instruments/"+InstrumentDataBase[index].nombre+".png",InstrumentDataBase[index].nombre, InstrumentDataBase[index].description);
             break;
             case RewardClass.upgrade:
-                super(scene,x,y,"../../assets/img/u_"+index+".png","","");
+                super(scene,x,y,"../../assets/img/upgrades/"+index+".png","","");
             break;
             case RewardClass.artifact:
-                super(scene,x,y,"../../assets/img/a_"+index+".png","","");
+                super(scene,x,y,"../../assets/img/artifacts/"+index+".png","","");
             break;
         }
     }
