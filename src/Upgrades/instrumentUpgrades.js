a933ff4270f15dd222d5ffa60535747fed3b7328
Izan <izandeve@ucm.es>
2024-11-02 16:51:54 +0100

    Instruments Upgrades with name and description

--- src/Upgrades/instrumentUpgrades.js
+++ src/Upgrades/instrumentUpgrades.js
@@ -1,16 +1,28 @@
 const InstrumentUpgrades = [
+    {
+        nombre: "Presteza",
+        description: "",
     //Reducir Cooldown en 1
-    function(instrument){
-        instrument.cooldown = Math.max(instrument.cooldown-1,1);
+        effectToApply: function(instrument){
+            instrument.cooldown = Math.max(instrument.cooldown-1,1);
+        },
     },
-    //Mejorar nota
-    function(instrument){
-        instrument.tipoNotas = Math.max(Math.min(instrument.tipoNotas+1, 2), 0);
-    }, 
-    //Aumentar numero notas
-    function(instrument){
-        instrument.numeroNotas++;
+    {
+        nombre: "Brio",
+        description: "",
+            //Mejorar nota
+        effectToApply: function(instrument){
+            instrument.tipoNotas = Math.max(Math.min(instrument.tipoNotas+1, 2), 0);
+        }
     },
+    {
+        nombre: "Salero",
+        description: "",
+        //Aumentar numero notas
+        effectToApply: function(instrument){
+            instrument.numeroNotas++;
+        },
+    }
 ]
 
 export default InstrumentUpgrades;
