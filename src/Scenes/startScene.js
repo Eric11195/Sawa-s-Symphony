4387d5827fbf28336c6d1ef02682202f9c6aa9b6
Óscar Melquiades Durán Narganes <osduran@ucm.es>
2024-12-12 23:31:29 +0100

    Arquitectura actualizada

--- src/Scenes/startScene.js
+++ src/Scenes/startScene.js
@@ -4,20 +4,17 @@ import RewardClass from "../DataDumpFiles/RewardClass.js";
 import { MidscreenX, MidscreenY } from "../Utils/screenPositions.js";
 export default class StartScene extends Phaser.Scene{
     player;
-    startReward;
     rewardChosen = false;
     nextBattleButton;
     constructor(){
         super({key: "startScene"});
-    }
-    preload(){
     }
     create(){
         this.player = new Player(this).setVisible(false);
         this.add.text(200,100, `Todo lo que eres. Menos de lo que serás.`,{fontFamily:"Grandstander",fontSize:"48px"}).setTint(0xe69138);
         this.nextBattleButton = this.add.image(MidscreenX(),500, "NextBattleButton").setDisplaySize(200,200).setInteractive().on("pointerdown", this.StartCombatScene, this).preFX.addColorMatrix();
         this.nextBattleButton.grayscale(1);
-        this.startReward = new Reward(this,{x:MidscreenX(), y:MidscreenY()-50}, RewardClass.instrument, 1, this.player, false, 0, this.OnRewardChosen.bind(this));
+        new Reward(this,{x:MidscreenX(), y:MidscreenY()-50}, RewardClass.instrument, 1, this.player, false, 0, this.OnRewardChosen.bind(this));
     }   
     StartCombatScene(){
         if (this.rewardChosen) 
